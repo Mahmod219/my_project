eaeeb9bfa4df9312e0c93d9f5105cd06bca09c6d
Mahmod219 <mahmod219@gmail.com>
2026-05-13 15:16:10 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+
+  // Requirement: Interactivity
+  header.addEventListener("click", () => {
+    alert("Thank you for visiting my portfolio!");
+  });
+});
